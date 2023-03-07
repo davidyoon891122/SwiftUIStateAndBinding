49b01a2cbd8e71952fa66259e769e6174306d980
David Yoon <qqpo12@naver.com>
2023-03-07 22:22:37 +0900

    Initial Commit

---
+++ StateAndBinding/StateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndBindingApp.swift
+//  StateAndBinding
+//
+//  Created by Jiwon Yoon on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct StateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
