c8f45697660579f4858553fdd1b0d4d6cb88f1f7
David Yoon <qqpo12@naver.com>
2023-03-08 20:07:12 +0900

    Add the swiftUI
    * Added a Image, ColorPicker, Slider
    * Used a State, Binding to pass data to subView(SwiftyControls)

--- StateAndBinding/ContentView.swift
+++ StateAndBinding/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var swiftyColor: Color = .red
+    @State var swiftyOpacity: Double = 0.7
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            SwiftyControls(
+                swiftyColor: $swiftyColor,
+                swiftyOpacity: $swiftyOpacity
+            )
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .padding(25)
+                .opacity(swiftyOpacity)
+                .foregroundColor(.white)
+                .background(swiftyColor)
+                .cornerRadius(50)
         }
         .padding()
     }
 }
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+    var body: some View {
+        VStack {
+            ColorPicker("Swifty Color", selection: $swiftyColor)
+            Slider(value: $swiftyOpacity, in:0...1)
+                .accentColor(swiftyColor)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
